4b3db85070086ae0d8c30736ff39d705c497f695
epicurean21 <jm_company@naver.com>
2021-11-29 23:12:56 +0900

    [Spring Boot Test] feat: first commit [lombok dependency 추가 & Field DI 삭제]

--- SpringBootTest/src/main/java/com/example/web/BasicController.java
+++ SpringBootTest/src/main/java/com/example/web/BasicController.java
@@ -2,24 +2,25 @@ package com.example.web;
 
 import com.example.domain.TestMessage;
 import com.example.service.BasicService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class BasicController {
 
-	@Autowired
-	private BasicService service;
+    private BasicService service;
 
-	@GetMapping("/test")
-	public String test(@RequestParam("flag") int flag) {
-		return service.test(flag);
-	}
+    @GetMapping("/test")
+    public String test(@RequestParam("flag") int flag) {
+        return service.test(flag);
+    }
 
-	@GetMapping("/jsonTest")
-	public TestMessage jsonTest() {
-		return service.jsonTest();
-	}
+    @GetMapping("/jsonTest")
+    public TestMessage jsonTest() {
+        return service.jsonTest();
+    }
 }
